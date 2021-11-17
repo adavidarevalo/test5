7ac628bd1848486252d982f7ca2ad211f84afa85
David Arevalo <davidarevaloc20@gmail.com>
2021-11-17 16:45:34 -0500

    Resolve the bug

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,12 +1,10 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Seo from "../components/Layout/seo"
-import headerPdf from '../components/Layout/headerPdf'
 
 const SecondPage = () => {
 return(
   <>
-    <headerPdf/>
     <Seo title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
