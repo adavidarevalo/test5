80d266a40c8a51d5ab75342ca5a43da2bd87cb9c
David Arevalo <davidarevaloc20@gmail.com>
2021-11-17 23:03:33 -0500

    test 8

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,10 @@
 import * as React from "react"
 import Layout from "../components/Layout/layout"
-import Seo from "../components/Layout/seo"
-import Main from '../components/Home/Main'
-import Abilities from '../components/Home/Abilities'
-import AboutMe from '../components/Shared/AboutMe'
-import Banner from '../components/Shared/Banner.jsx'
 
 const IndexPage = () =>{
   return(
     <Layout>
-      <Seo/>
-      <Main/>
-      <AboutMe/>
-      <Abilities/>
-      <Banner/>
+      <p>Test</p>
     </Layout>
   )
 } 
